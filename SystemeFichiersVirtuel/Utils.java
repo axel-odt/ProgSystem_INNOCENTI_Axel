5fced91618a56bfcf3140a30c8395a6fb00186ae
AXEL INNOCENTI <axel.innocenti@iut.local>
2026-09-16 09:16:57 +0200

    Ajout TP2

---
+++ SystemeFichiersVirtuel/Utils.java
@@ -0,0 +1,32 @@
+public class Utils {
+
+    public static int writeInt(byte[] memory, int offset, int value) {
+        // TODO: Écrire les 4 octets de 'value' dans 'memory'
+        // à partir de 'offset', en big-endian.
+	//int curseur = 0;
+	//curseur += write.Int(memory, curseur, int);
+        memory[offset]     = (byte) (value >>> 24);
+        memory[offset + 1] = (byte) (value >>> 16);
+        memory[offset + 2] = (byte) (value >>> 8);
+        memory[offset + 3] = (byte) value;
+
+        return 4;
+    }
+
+    public static int readInt(byte[] memory, int offset) {
+        // TODO: Reconstituer le int sur 4 octets.
+	return 0;
+    }
+
+    public static int writeShort(byte[] memory, int offset, short value) {
+        // TODO: Écrire les 2 octets de 'value'.
+//	memory[offset]     = (byte) (value >>> 24);
+//      memory[offset + 1] = (byte) (value >>> 16);
+        return 2;
+    }
+
+    public static short readShort(byte[] memory, int offset) {
+        // TODO: Lire le short sur 2 octets.
+        return 0;
+    }
+}
